10ad2cec8fa53d1cb991a37b1557cbc829eaa716
Husseini <mhmd.husseinyy@gmail.com>
2023-12-24 21:02:12 +0200

    add validation rules

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,35 +1,42 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
+import mongoose from 'mongoose'
+import validator from 'validator'
+import bcrypt from 'bcryptjs'
 
-const userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     name: {
       type: String,
-      required: true,
+      required: [true, 'Name is required'],
     },
     email: {
       type: String,
-      required: true,
+      required: [true, 'Email is required'],
       unique: true,
+      lowercase: true, 
+      validate: {
+        validator: validator.isEmail, 
+        message: 'Please provide a valid email address',
+      },
     },
     password: {
       type: String,
-      required: true,
+      required: [true, 'Password is required'],
+      minlength: [8, 'Password must be at least 8 characters long'],
     },
-  },{timestamps: true}
-);
+  },{ timestamps: true }
+)
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
-};
+}
 
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
     next();
   }
-
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
-});
+})
 
 const User = mongoose.model('User', userSchema);
 
