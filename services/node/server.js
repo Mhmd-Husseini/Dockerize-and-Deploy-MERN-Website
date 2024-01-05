c0fa1682bb5983b169236f95e2727d21ab71410a
Husseini <mhmd.husseinyy@gmail.com>
2024-01-05 02:10:48 +0200

    finalizing production dockerizing

--- services/node/server.js
+++ services/node/server.js
@@ -6,24 +6,24 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 dotenv.config()
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3001
 connectDB()
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/users', userRoutes)
 
-if (process.env.NODE_ENV === 'production') {
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
-    app.get('*', (req, res) =>
-      res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
-    );
-  } else {
-    app.get('/', (req, res) => {
-      res.send('API is running....');
-    });
-}
+// if (process.env.NODE_ENV === 'production') {
+//     const __dirname = path.resolve();
+//     app.use(express.static(path.join(__dirname, '/frontend/dist')));
+//     app.get('*', (req, res) =>
+//       res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
+//     );
+//   } else {
+//     app.get('/', (req, res) => {
+//       res.send('API is running....');
+//     });
+// }
 
 app.use(notFound)
 app.use(errorHandler)
